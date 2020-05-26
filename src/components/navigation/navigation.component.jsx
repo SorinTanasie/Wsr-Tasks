b34f867e41b3de462eccac8935f49ab3314869f6
Sorin <tanasie.sorin1@gmail.com>
2020-05-26 15:32:05 +0200

    Added firebase

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navigation.styles.scss'
+
+const Navigation =() =>(
+    <div className="navigation">
+        <div>Task</div>
+        <div>Analitics</div>
+    </div>
+)
+
+export default Navigation;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = () =>(
+    <div className="homepage">
+        <h1>HOMEPAGE</h1>
+    </div>
+)
+export default Homepage;
