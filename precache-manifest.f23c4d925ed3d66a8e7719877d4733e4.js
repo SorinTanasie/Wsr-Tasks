48e76e9ea0332b7e714f66e0d653e437414015d9
mr robot <geicarobert@gmail.com>
2020-06-03 20:53:35 +0300

    Updates

---
+++ precache-manifest.f23c4d925ed3d66a8e7719877d4733e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85265acc7e9919474fe62d92dace0903",
+    "url": "/Wsr-Tasks/index.html"
+  },
+  {
+    "revision": "6f623499117b19f0a9be",
+    "url": "/Wsr-Tasks/static/css/main.79d4e618.chunk.css"
+  },
+  {
+    "revision": "8605c147649f019c053b",
+    "url": "/Wsr-Tasks/static/js/2.e0d66110.chunk.js"
+  },
+  {
+    "revision": "f526aa5e1b6f074a28fbd27593ba35b3",
+    "url": "/Wsr-Tasks/static/js/2.e0d66110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f623499117b19f0a9be",
+    "url": "/Wsr-Tasks/static/js/main.036e8c68.chunk.js"
+  },
+  {
+    "revision": "7e5a3ca0b6cea6f5cec8",
+    "url": "/Wsr-Tasks/static/js/runtime-main.e0593a40.js"
+  }
+]);
