f01416a5ef4d2e8842e6a0eed568fc050ec193f1
mr robot <geicarobert@gmail.com>
2020-06-08 18:49:04 +0300

    add employee

--- src/pages/User/EmployeeModal.js
+++ src/pages/User/EmployeeModal.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from 'react-modal';
 import './employeemodal.scss';
 import Autocomplete from 'react-autocomplete';
 
+import {firestore} from '../../firebase/firebase';
+
+
 const EmployeeModal = ({toggle, handleCloseModal}) => {
 
+
+
+    const [user, setUser] = useState({});
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('');
+
+  
+
+    const handleSubmitUser = (e) => {
+        e.preventDefault();
+        
+        if(!name && !email) return;
+        if(name.trim() === '' && email.trim() === '') return;
+        
+        addUser(name, email);
+        setName = "";
+        setEmail = "";
+    }
+
+
+   
+    const docRef = firestore.collection('users-test');
+
+
+    const addUser = async (name, email) => {
+        console.log(name);
+        console.log(email);
+
+        if(name !== undefined && email !== undefined) {
+            await docRef.add({name, email}).then(docRef => {
+                console.log('employee added with id: ' + docRef.id);
+            }).catch(err => {
+                console.log('error adding new employee: ', err);
+            })
+        }
+
+           
+    }
+
+
+    
     return (
         <Modal
-            isOpen={!!toggle}
+            isOpen={!toggle}
             contentLabel="Task Modal"
             onRequestClose={handleCloseModal}
             ariaHideApp={false}
@@ -19,11 +63,22 @@ const EmployeeModal = ({toggle, handleCloseModal}) => {
             <div className="employeeModal-body">
                 <h1>Add Employee</h1>
                 
-                <form>
-                    <input type="text" name="name" placeholder="Nume angajat" />
-                    <input type="text" name="name" placeholder="Email angajat" />
+                <form onSubmit={handleSubmitUser}>
+                    <input 
+                        type="text" 
+                        placeholder="Nume angajat" 
+                        value={name} 
+                        onChange={e => setName(e.target.value)}
+                        />
+
+                    <input 
+                        type="text" 
+                        placeholder="Email angajat" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     
-                    <button className="button">Adauga angajat</button>
+                    <button className="button" onClick={addUser}>Adauga angajat</button>
                 </form>
 
 
